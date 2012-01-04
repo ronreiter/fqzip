9a3d9634c7f2ea53995088c3e0833d4a94ae82c0
Barak Jacob <baonly@gmail.com>
2012-01-04 21:24:05 +0200

    dsd

--- java/src/ContextHasher.java
+++ java/src/ContextHasher.java
@@ -1,3 +1,6 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,29 +16,39 @@ public class ContextHasher {
     private final int QUALITY_BACKWARDS = 3;
     private final int POSITION_DIVISION = 1;
     private final int QUALITY_DIVISION = 1;
-
     
     ContextHasher() {
         
     }
 
-    String hashContext(int position, String sequenceContext, String qualityContext) {
-        String context = "" + (position / POSITION_DIVISION);
+     String hashContext(int position, String sequenceContext, String qualityContext) {
+        String context = "" + (position / POSITION_DIVISION) + ":";
     
         for (int i = position - SEQUENCE_BACKWARDS; i < Math.min(position + SEQUENCE_FORWARD, 100); i++) {
             context += sequenceContext.charAt(i);
         }
-    
-        for (int i = position - QUALITY_BACKWARDS; i < position + QUALITY_BACKWARDS; i++) {
-                context += sequenceContext.charAt(i);
+        context += ":";
+
+
+
+        List<Integer> qualities = new ArrayList<Integer> ();
+        for (int i = Math.max((position - QUALITY_BACKWARDS), 0); i < position + QUALITY_BACKWARDS; i++) {
+
+            qualities.add((((int)qualityContext.charAt(i) - 33) / QUALITY_DIVISION));
         }
+        context += ":" + Utils.join(qualities,",");
+
+        System.out.println(context);
          return context;
+
+
             
     }
-
-      
-              
     
+    public static void main (String[] args) {
+        new ContextHasher().hashContext(10, "AGTAACGAGAGATCTACGACTGCATCACTACGACTACGACTCGACTACTG", "$%%^^$GSDSDSD$SA$SAS$^S$SA#A$SAS^ASA$S^A");
+    }
 
 
 }
+
---
+++ java/src/Utils.java
@@ -0,0 +1,29 @@
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: barakjacob
+ * Date: 1/4/12
+ * Time: 8:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Utils {
+
+
+
+
+    public static String join(Collection<?> s, String delimiter) {
+        StringBuilder builder = new StringBuilder();
+        Iterator iter = s.iterator();
+        while (iter.hasNext()) {
+            builder.append(iter.next());
+            if (!iter.hasNext()) {
+                break;
+            }
+            builder.append(delimiter);
+        }
+        return builder.toString();
+    }
+}
+
