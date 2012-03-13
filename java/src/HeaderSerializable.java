87151057c2e4fc9366d55f48cb1bb3c20d04efd1
Ron Reiter <ron@Rons-MacBook-Air.local>
2012-03-13 17:57:41 +0200

    headerserializable

--- java/src/HeaderSerializable.java
+++
@@ -1,9 +0,0 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-public interface HeaderSerializable {
-	public void serialize(DataOutputStream stream) throws IOException;
-
-	public void parse(DataInputStream stream) throws IOException;
-}
